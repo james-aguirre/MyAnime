6b86d4a03a3bdc63486bf4019f5f13a161ee2ed8
James Aguirre <jamesaguirre98@gmail.com>
2023-04-25 16:29:38 +0000

    js data

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+
+var data = {
+  view: 'entry-form',
+  entries: [],
+  nextEntryId: 1
+};
+
+window.addEventListener('beforeunload', function (event) {
+  const jSon = JSON.stringify(data);
+  this.localStorage.setItem('javascript-local-storage', jSon);
+}
+);
+
+const previousNotes = localStorage.getItem('javascript-local-storage');
+if (previousNotes !== null) {
+  data = JSON.parse(previousNotes);
+}
